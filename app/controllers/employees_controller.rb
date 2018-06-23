6a0db5393ae60e5a727eb69a44311f66d6552f20
belenac <eac1992@gmail.com>
2018-06-22 20:28:37 -0400

    Empresa, Area, Empleado

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,19 @@
+class EmployeesController < ApplicationController
+  def create
+    @employee = Employee.new(employee_params)
+    # @employee.area = Area.find(@employee.area)
+    @employee.save
+    redirect_to @employee.company
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to @employee.company
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :area_id, :company_id)
+  end
+end
